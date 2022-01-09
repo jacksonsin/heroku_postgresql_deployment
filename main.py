223c77920c369da1c81e50edee2eea7caed8f621
Jackson <jackson.sin83@gmail.com>
2022-01-09 10:24:57 +0800

    Final

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
-import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hnnwinwflpkjdq:2f0721a5d89894008beb0299136fd7acfa1e016815135bc94adfd1b274056c97@ec2-54-221-82-166.compute-1.amazonaws.com:5432/d94p3l6dfsa1ob'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = 'password'
 
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-class User(db.Model):
-    __tablename__='users'
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    eid = db.Column(db.String(120), nullable=False)
 
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String,unique=True)
-    email=db.Column(db.String,unique=True)
+    def __init__(self, name, eid):
+        self.name = name
+        self.eid = eid
 
-db.init_app(app)
+@app.route('/')
+def addperson():
+    return render_template("index.html")
 
-@app.route("/", methods=['GET','POST'])
-def new():
-    if request.method=="POST":
-        email=request.form['email']
-        name=request.form['name']
+@app.route("/personadd", methods=['POST'])
+def personadd():
+    name = request.form["name"]
+    eid = request.form["eid"]
+    entry = People(name, eid)
+    db.session.add(entry)
+    db.session.commit()
 
-        user=User(name=name,email=email)
-        db.session.add(user)
-        db.session.commit()
-    return render_template("new.html")
+    return render_template("index.html")
 
-@app.route("/user/<username>")
-def users(username):
-    user = User.query.filter_by(name=username)
-    if user is None:
-        return "No user found"
-    else:
-        return user.email
+if __name__ == '__main__':
+    app.debug = False
+    db.create_all()
+    app.run()
