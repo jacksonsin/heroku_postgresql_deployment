c3536761f4d6e7b4d37279e6da1fdf886c20fb95
Jackson Sin <35521578+jacksonsin@users.noreply.github.com>
2022-08-10 14:42:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hnnwinwflpkjdq:2f0721a5d89894008beb0299136fd7acfa1e016815135bc94adfd1b274056c97@ec2-54-221-82-166.compute-1.amazonaws.com:5432/d94p3l6dfsa1ob'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://<aws resources>'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = 'password'
 
